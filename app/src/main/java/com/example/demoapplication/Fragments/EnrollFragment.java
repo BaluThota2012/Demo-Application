4f000776e38baeaf6bf34e274c547b153dfbb401
BaluThota2012 <68321999+BaluThota2012@users.noreply.github.com>
2021-02-19 22:36:52 +0530

    Update EnrollFragment.java

--- app/src/main/java/com/example/demoapplication/Fragments/EnrollFragment.java
+++ app/src/main/java/com/example/demoapplication/Fragments/EnrollFragment.java
@@ -83,72 +83,6 @@ public class EnrollFragment extends Fragment {
         date=binding.etDob;
 
 
-//        TextWatcher tw = new TextWatcher() {
-//
-//            private String current = "";
-//            private String ddmmyyyy = "DDMMYYYY";
-//            private Calendar cal = Calendar.getInstance();
-//
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//
-//                if (!charSequence.toString().equals(current)) {
-//                    String clean = charSequence.toString().replaceAll("[^\\d.]|\\.", "");
-//                    String cleanC = current.replaceAll("[^\\d.]|\\.", "");
-//
-//                    int cl = clean.length();
-//                    int sel = cl;
-//                    for (int j = 2; j <= cl && j < 6; j += 2) {
-//                        sel++;
-//                    }
-//                    //Fix for pressing delete next to a forward slash
-//                    if (clean.equals(cleanC)) sel--;
-//
-//                    if (clean.length() < 8) {
-//                        clean = clean + ddmmyyyy.substring(clean.length());
-//                    } else {
-//                        //This part makes sure that when we finish entering numbers
-//                        //the date is correct, fixing it otherwise
-//                        int day = Integer.parseInt(clean.substring(0, 2));
-//                        int mon = Integer.parseInt(clean.substring(2, 4));
-//                        int year = Integer.parseInt(clean.substring(4, 8));
-//
-//                        mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
-//                        cal.set(Calendar.MONTH, mon - 1);
-//                        year = (year < 1900) ? 1900 : (year > 2100) ? 2100 : year;
-//                        cal.set(Calendar.YEAR, year);
-//                        // ^ first set year for the line below to work correctly
-//                        //with leap years - otherwise, date e.g. 29/02/2012
-//                        //would be automatically corrected to 28/02/2012
-//
-//                        day = (day > cal.getActualMaximum(Calendar.DATE)) ? cal.getActualMaximum(Calendar.DATE) : day;
-//                        clean = String.format("%02d%02d%02d", day, mon, year);
-//                    }
-//
-//                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
-//                            clean.substring(2, 4),
-//                            clean.substring(4, 8));
-//
-//                    sel = sel < 0 ? 0 : sel;
-//                    current = clean;
-//                    date.setText(current);
-//                    date.setSelection(sel < current.length() ? sel : current.length());
-//                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//
-//            }
-//        };
-
-
-
         dialog = new ProgressDialog(getActivity());
         dialog.setTitle("Adding User");
         dialog.setMessage("user is being added...");
@@ -212,19 +146,6 @@ public class EnrollFragment extends Fragment {
                 }
 
 
-                String regx1 ="[A-Z][a-z]*";
-             //   String regx2 = "[A-Z][a-z]*";
-                Pattern pattern2=Pattern.compile(regx1);
-             //   Pattern pattern3=Pattern.compile(regx2);
-                Matcher matcher2=pattern2.matcher(firstname);
-              //  Matcher matcher3=pattern3.matcher(secondname);
-                boolean bool2=matcher2.matches();
-               // boolean bool3=matcher3.matches();
-
-                if(!bool2){
-                    dialog.dismiss();
-                    binding.etFirstname.setError("firstname does not met naming requirements");
-                }
                 if(TextUtils.isEmpty(firstname)){
                     dialog.dismiss();
                     binding.etFirstname.setError("enter your first name");
@@ -387,21 +308,5 @@ public class EnrollFragment extends Fragment {
 
         }
     }
-    private String getAge(int year, int month, int day){
-        Calendar dob = Calendar.getInstance();
-        Calendar today = Calendar.getInstance();
-
-        dob.set(year, month, day);
-
-        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
-
-        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)){
-            age--;
-        }
-
-        Integer ageInt = new Integer(age);
-        String ageS = ageInt.toString();
-
-        return ageS;
-    }
+  
 }
